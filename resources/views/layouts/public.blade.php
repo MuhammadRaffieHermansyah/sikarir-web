5682ac6de03d098a3c5423f6ea1529646c462197
Ilham Mubarok <ilhmmbrok@gmail.com>
2026-09-22 21:24:17 +0700

    auth: role middleware and route

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -6,13 +6,9 @@
     <title>@yield('title', 'siKarir - UPT BLK Jember')</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet" />
-
-    <!-- CDN Lucide Icons (WAJIB ADA) -->
-    <script src="https://unpkg.com/lucide@latest"></script>
-
+    @fonts
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://unpkg.com/lucide@latest"></script>
 </head>
 <body class="font-sans antialiased text-slate-800 bg-white">
 
